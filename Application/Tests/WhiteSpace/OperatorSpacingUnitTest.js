41ea4312f1254717c02c86692386ffeac8676c3b
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-06-13 11:28:37 +0200

    custom operator spacing sniff that doesn't check double arrows

---
+++ Application/Tests/WhiteSpace/OperatorSpacingUnitTest.js
@@ -0,0 +1,91 @@
+
+
+result = 1 + 2;
+result = 1  + 2;
+result = 1  +   2;
+result = 1 +2;
+result = 1+ 2;
+result = 1+2;
+
+result = 1 - 2;
+result = 1  - 2;
+result = 1  -   2;
+result = 1 -2;
+result = 1- 2;
+result = 1-2;
+
+result = 1 * 2;
+result = 1  * 2;
+result = 1  *   2;
+result = 1 *2;
+result = 1* 2;
+result = 1*2;
+
+result = 1 / 2;
+result = 1  / 2;
+result = 1  /   2;
+result = 1 /2;
+result = 1/ 2;
+result = 1/2;
+
+result = 1 % 2;
+result = 1  % 2;
+result = 1  %   2;
+result = 1 %2;
+result = 1% 2;
+result = 1%2;
+result = '100%';
+
+result += 4;
+result+=4;
+result -= 4;
+result-=4;
+result /= 4;
+result/=4;
+result *=4;
+result*=4;
+
+$.localScroll({offset: {top: -32}});
+
+switch (result) {
+	case -1:
+		break;
+}
+
+result = x?y:z;
+result = x ? y : z;
+
+if (something === true
+    ^ somethingElse === true
+) {
+    return false;
+}
+// @codingStandardsChangeSetting Application.WhiteSpace.OperatorSpacing ignoreNewlines false
+y = 1
+  + 2  
+  - 3;
+
+y = 1 + 
+    2 - 
+    3;
+
+y = 1
++ 2
+- 3;
+
+// @codingStandardsChangeSetting Application.WhiteSpace.OperatorSpacing ignoreNewlines true
+y = 1
+  + 2
+  - 3;
+
+y = 1 +
+    2 -
+    3;
+
+y = 1
++ 2
+- 3;
+// @codingStandardsChangeSetting Application.WhiteSpace.OperatorSpacing ignoreNewlines false
+
+if (true || -1 == b) {
+}
